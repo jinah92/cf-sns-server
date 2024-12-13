1720c3008544c709a0d848d9d2dd45b389cefdca
jinah92 <jinah0818@gmail.com>
2024-12-13 14:14:57 +0900

    feat: serialize intercepter 주석 해제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,7 +11,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  // @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(ClassSerializerInterceptor)
   /**
    * serialization(직렬화): 현재 시스템에서 사용되는 nest.js 데이터의 구조를 다른 시스템에서도 쉽게 사용 할 수 있는 포맷으로 변환
    *                      => class의 object에서 JSON 포맷으로 변환
