f2136a0a6edbcce07e96ad7c6aaad26b58783f8a
jinah92 <jinah0818@gmail.com>
2024-12-13 17:17:17 +0900

    feat: 공용 paginate함수 구성 및 compositeFindOptions 구현

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { BasePaginationDto } from './entity/base-pagination.dto';
-import { FindManyOptions, Repository } from 'typeorm';
+import {
+  FindManyOptions,
+  FindOptionsOrder,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 import { BaseModel } from './entity/base.entity';
+import { FILTER_MAPPER } from './const/filter-mapper.const';
 
 @Injectable()
 export class CommonService {
@@ -10,5 +16,110 @@ export class CommonService {
     repository: Repository<T>,
     overrideFindOptions: FindManyOptions<T> = {},
     path: string,
+  ) {
+    return dto.page
+      ? this.pagePaginate(dto, repository, overrideFindOptions)
+      : this.cursorPaginate(dto, repository, overrideFindOptions, path);
+  }
+
+  private async pagePaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
   ) {}
+
+  private async cursorPaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string,
+  ) {
+    const findOptions = this.composeFindOptions<T>(dto);
+  }
+
+  private composeFindOptions<T extends BaseModel>(
+    dto: BasePaginationDto,
+  ): FindManyOptions<T> {
+    /**
+     * where,
+     * order,
+     * take,
+     * skip => page 기반인 경우에만
+     *
+     * 1) where로 시작하면 필터 로직 적용
+     * 2) order로 시작하면 정렬 로직 적용
+     * 3) 필터 로직을 적용한다면 '__" 기준으로 split했을 때 3개의 값으로 나뉘는지, 2개의 값으로 나뉘는지 확인
+     *   3-1) 3개의 값으로 나뉘면 FILTER_MAPPER에서 해당되는 operator 함수를 찾아서 적용한다
+     *       ex. ['where', 'id', 'more_than']
+     *   3-2) 2개의 값으로 나뉘면 정확한 값을 필터하는 경우 (opertaor 사용 X)
+     *       ex. ['where', 'id']
+     * 4) order의 경우 3-2 방법을 적용한다
+     */
+    let where: FindOptionsWhere<T> = {};
+    let order: FindOptionsOrder<T> = {};
+
+    for (const [key, value] of Object.entries(dto)) {
+      if (key.startsWith('where__')) {
+        where = {
+          ...where,
+          ...this.parseFilter(key, value),
+        };
+      } else if (key.startsWith('order__')) {
+        order = {
+          ...order,
+          ...this.parseFilter(key, value),
+        };
+      }
+    }
+
+    return {
+      where,
+      order,
+      take: dto.take,
+      skip: dto.page ? dto.take * (dto.page - 1) : null,
+    };
+  }
+
+  private parseFilter<T extends BaseModel>(
+    key: string,
+    value: any,
+  ): FindOptionsWhere<T> {
+    const options: FindOptionsWhere<T> = {};
+
+    /**
+     * ex. where__id__more_than => ['where', 'id', 'more_than']
+     */
+    const split = key.split('__');
+
+    if (split.length !== 2 && split.length !== 3) {
+      throw new BadRequestException(
+        `where 필터는 '__'로 split 했을 때 길이가 2 또는 3이어야 합니다. key: ${key}`,
+      );
+    }
+
+    if (split.length === 2) {
+      const [_, field] = split;
+      options[field] = value;
+    } else {
+      /**
+       * split 길이가 3인 경우, Typeorm 유틸리티 적용이 필요하다.
+       *
+       * ex. where__id__more_than인 경우, where는 버려도 되고 / id는 키값이 되고 / more_than은 typeorm 유틸리티가 된다.
+       *
+       * FILTER_MAPPER에 미리 정의해둔 값들로
+       * feild 값에 FILTER_MAPPER에서 해당되는 utility를 가져온 후 값에 적용해준다.
+       */
+      const [_, field, oprator] = split;
+
+      // where__id__between = 3, 4
+      // 만약 split 대상 문자가 존재하지 않으면 길이가 무조건 1이다
+      // const values = value.toString().split(',');
+
+      // filed => id, operator => more_than
+      // FILTER_MAPPER[operator] => MoreThan
+      options[field] = FILTER_MAPPER[oprator](value);
+    }
+
+    return options;
+  }
 }
