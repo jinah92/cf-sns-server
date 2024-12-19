cd4748a66b805c714913f2c15b8eecdfa5bc3f20
jinah92 <jinah0818@gmail.com>
2024-12-19 11:13:16 +0900

    feat: room을 활용하여 메시지 전달

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,23 +1,45 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({
   // ws://localhost:3001/chats
   namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
   handleConnection(socket: Socket) {
     console.log(`on connect called: ${socket.id}`);
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    // 방의 chat id를 리스트로 받는다
+    @MessageBody() data: number[],
+    @ConnectedSocket() socket: Socket,
+  ) {
+    for (const chatId of data) {
+      // socket.join()
+      socket.join(chatId.toString());
+    }
+  }
+
   // socket.on('send_message', message => console.log(message));
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string) {
-    console.log(message);
+  sendMessage(
+    @MessageBody() message: { message: string; chatId: number },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    this.server
+      .in(message.chatId.toString())
+      .emit('receive_message', 'hello from server');
   }
 }
